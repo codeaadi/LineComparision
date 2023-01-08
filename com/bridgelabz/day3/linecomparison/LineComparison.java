586720b662ee572351793e3e8e5da626ae70bbde
codeaadi <adityadubey362@gmail.com>
2023-01-08 19:22:38 +0530

    Added: Welcome message for Line Comparison

---
+++ com/bridgelabz/day3/linecomparison/LineComparison.java
@@ -0,0 +1,6 @@
+package com.bridgelabz.day3.linecomparison;
+
+public class LineComparison {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Line Comparison Program ");
+}}
